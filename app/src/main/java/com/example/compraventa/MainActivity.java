3800bd7382075f2ccfc5a25d2944b46809e96349
IgnacioRomang <51174067+IgnacioRomang@users.noreply.github.com>
2021-09-02 21:54:30 -0300

    Cambio de algunas variables y cambiar los Listener por expresiones lamdba

--- app/src/main/java/com/example/compraventa/MainActivity.java
+++ app/src/main/java/com/example/compraventa/MainActivity.java
@@ -1,27 +1,20 @@
 package com.example.compraventa;
 
-import android.annotation.SuppressLint;
 import android.support.design.widget.BaseTransientBottomBar;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.InputFilter;
-import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayAdapter<CharSequence> spnstring;
@@ -49,43 +42,40 @@ public class MainActivity extends AppCompatActivity {
         spnstring= ArrayAdapter.createFromResource(this,R.array.categorias,R.layout.support_simple_spinner_dropdown_item);
         categorias.setAdapter(spnstring);
         //liseners
-        categorias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        categorias.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                //guardo la categoria elegida;
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 categElegida= i;
             }
-        });
-        retiro.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isCheck) {
-                LinearLayout retlay= findViewById(R.id.DireccionTodo);
-                if (isCheck) {
-                    retlay.setVisibility(View.VISIBLE);
-                } else {
-                    retlay.setVisibility(View.GONE);
-                }
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                categElegida=-1;
             }
         });
-        activDescuento.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isCheck) {
-                LinearLayout retlay= findViewById(R.id.SeekbarTodo);
-                TextView adv = findViewById(R.id.textAdv);
-                if (isCheck) {
-                    adv.setVisibility(View.VISIBLE);
-                    retlay.setVisibility(View.VISIBLE);
-                } else {
-                    adv.setVisibility(View.GONE);
-                    retlay.setVisibility(View.GONE);
-                }
+        retiro.setOnCheckedChangeListener((compoundButton, isCheck) -> {
+            LinearLayout retlay= findViewById(R.id.DireccionTodo);
+            if (isCheck) {
+                retlay.setVisibility(View.VISIBLE);
+            } else {
+                retlay.setVisibility(View.GONE);
+            }
+        });
+        activDescuento.setOnCheckedChangeListener((compoundButton, isCheck) -> {
+            LinearLayout retlay= findViewById(R.id.SeekbarTodo);
+            TextView adv = findViewById(R.id.textAdv);
+            if (isCheck) {
+                adv.setVisibility(View.VISIBLE);
+                retlay.setVisibility(View.VISIBLE);
+            } else {
+                adv.setVisibility(View.GONE);
+                retlay.setVisibility(View.GONE);
             }
         });
         descuento.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                Integer porcentaje= i;
-                textP.setText(porcentaje.toString());
+                textP.setText(i);
             }
 
             @Override
@@ -96,8 +86,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 TextView adv = findViewById(R.id.textAdv);
-                Integer text = 0;
-                if(textP.getText() == text.toString()){
+               //int text = 0;
+                if(textP.getText() == ((Integer)0).toString()){
                     adv.setVisibility(View.VISIBLE);
                 }
                 else{
@@ -105,21 +95,15 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        eula.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
-                    bpublicar.setClickable(true);
-                }
-                else{
-                    bpublicar.setClickable(false);
-                }
-            }
-        });
-        bpublicar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO hacer validacion
+        eula.setOnCheckedChangeListener((compoundButton, b) -> bpublicar.setClickable(b));
+        bpublicar.setOnClickListener(view -> {
+            //TODO hacer validacion
+
+            boolean validacion=false;
+
+            if(validacion){
+                //pasar a otra pantalla
+            }else{
                 Snackbar.make(view ,R.string.error_msj, BaseTransientBottomBar.LENGTH_LONG).setAction(R.string.error_acc,null).show();
             }
         });
